944da51bf2cfc3536f7bb5f90fe042c2a3f0a7c7
BlakeDiaz <176447188+BlakeDiaz@users.noreply.github.com>
2024-07-12 21:42:33 -0700

    Add macro to internal dynamic array
    Added macro to append to a dynamic array using a ctd_allocator

--- include/ctd_internal_dynamic_array.h
+++ include/ctd_internal_dynamic_array.h
@@ -38,4 +38,33 @@
         }                                                                                                              \
     } while (0)
 
+#define ctd_internal_dynamic_array_append_with_allocator(array, type, item, allocator, error_ptr)                      \
+    do                                                                                                                 \
+    {                                                                                                                  \
+        void* _new_data;                                                                                               \
+        if ((array).length == (array).capacity)                                                                        \
+        {                                                                                                              \
+            _new_data = (allocator).reallocate((allocator).context, (array).data, (array).capacity * sizeof(type),     \
+                                             ((array).capacity * 2 + 1) * sizeof(type), alignof(type));                \
+            if (_new_data == NULL)                                                                                     \
+            {                                                                                                          \
+                (allocator).deallocate((allocator).context, (array).data, (array).capacity * sizeof(type));            \
+                (error_ptr)->error_type = ALLOCATION_FAIL;                                                             \
+                (error_ptr)->error_message = "Realloc failed.";                                                        \
+            }                                                                                                          \
+            else                                                                                                       \
+            {                                                                                                          \
+                (array).data = _new_data;                                                                              \
+                (array).data[(array).length] = item;                                                                   \
+                (array).length++;                                                                                      \
+                (array).capacity = (array).capacity * 2 + 1;                                                           \
+            }                                                                                                          \
+        }                                                                                                              \
+        else                                                                                                           \
+        {                                                                                                              \
+            (array).data[(array).length] = item;                                                                       \
+            (array).length++;                                                                                          \
+        }                                                                                                              \
+    } while (0)
+
 #endif // CTD_INTERNAL_DYNAMIC_ARRAY_H
